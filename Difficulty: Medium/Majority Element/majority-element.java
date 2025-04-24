1011e663feb37bf3cd1b67ef235842c48a96381b
Pulkit Kumar Mathur <97748031+Pulkit1822@users.noreply.github.com>
2025-04-25 01:20:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
+            System.out.println(new Solution().majorityElement(arr));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static int majorityElement(int arr[]) {
+        if(arr.length==1)
+        return arr[0];
+        int size=arr.length/2;
+        int count=1;
+        Arrays.sort(arr);
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]==arr[i-1]){
+            count++;
+                if(count>size)
+                return arr[i];
+            }
+            else
+            count=1;
+        }
+        return -1;
+    }
+}
